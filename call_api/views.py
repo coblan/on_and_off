dff3f0720232177e3d63a08554aab1f13c049383
heyulin <coblan@163.com>
2018-05-20 16:18:46 +0800

    基本完成

--- call_api/views.py
+++ call_api/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,HttpResponse
+import requests
+import json
+from django.views.decorators.csrf import csrf_exempt
+import dicttoxml
 # Create your views here.
+
+@csrf_exempt
 def call_api_page(request):
-    
-    return render(request,'call_api/index.html')
+    if request.method=='GET':
+        return render(request,'call_api/index.html')
+    else:
+        dc = json.loads(request.body)
+        fields = dc.get('fields')
+        row  = dc.get('row')
+        normed_row = {f:row.get(f) for f in fields}
+        xml = dicttoxml.dicttoxml(normed_row,custom_root='REQUEST')
+        
+        try:
+            rt = requests.post(dc.get('rq_url'),data=xml)
+            if rt.status_code == 200:
+                return HttpResponse(rt.context)
+            else:
+                return HttpResponse('error code is %s'%rt.status_code)
+        except requests.exceptions.RequestException as e:
+            return HttpResponse('Exception : %s'%str(e))
+        
